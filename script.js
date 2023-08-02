50befa378cbe5ed5f9c3bd27407e4b7581bbb434
Xentatonix <14041598+Xentatonix@users.noreply.github.com>
2023-08-02 21:38:26 +0100

    Completed winner calculation logic
    Checks game scenario through array and then creates message

--- script.js
+++ script.js
@@ -6,23 +6,59 @@
 
 let getComputerChoice = () => {
     let randomNumber = Math.random()*3
-
-/*     if (randomNumber < 1) {
-        return "rock"
-    } else if (randomNumber >= 1 && randomNumber < 2) {
-        return "paper"
-    } else if (randomNumber >= 2 && randomNumber < 3) {
-        return "scissors"
-    } */
-
-    return randomNumber < 1 ? "rock" : 
-        randomNumber >= 1 && randomNumber < 2 ? "paper" : 
-            "scissors"
-
+    return randomNumber < 1 ? "rock" : randomNumber >= 1 && randomNumber < 2 ? "paper" : "scissors"
 }
 
-console.log(getComputerChoice())
+let computerChoice = getComputerChoice();
+console.log(computerChoice);
 
 // Human selects rock, paper or scissors 
+
+let getHumanChoice = () => prompt("Please choose rock, paper or scissors").toLowerCase();
+
+let humanChoice = getHumanChoice();
+
+while (!(["rock", "paper", "scissors"].includes(humanChoice))) {
+    getHumanChoice()
+}
+
 // Winner is decided
+
+let playRound = (computerChoice, humanChoice) => {
+    // Create game conditions:
+
+    // if array contains rock and paper
+        // if humanChoice paper, human wins else computer wins
+    // if array contains paper and scissors
+        // if humanChoice scissors, human wins else computer wins
+    // if array contains scissors and rock
+        // if humanChoice rock, human wins else computer wins
+
+    let gameArray = [humanChoice, computerChoice];
+    
+    let outcome;
+    let gameCondition;
+
+    if (humanChoice === computerChoice) {
+        outcome = "Draw!";
+        gameCondition = `${humanChoice} matches ${computerChoice}`;
+
+    } else if (gameArray.includes("rock") && gameArray.includes("paper")) {
+        outcome = humanChoice === "paper" ? "You win!" : "You lose!";
+        gameCondition = "Paper beats rock";
+
+    } else if (gameArray.includes("paper") && gameArray.includes("scissors")) {
+        outcome = humanChoice === "scissors" ? "You win!" : "You lose!"
+        gameCondition = "Scissors beats paper";
+
+    } else if (gameArray.includes("scissors") && gameArray.includes("rock")) {
+        outcome = humanChoice === "rock" ? "You win!" : "You lose!"
+        gameCondition = "Rock beats scissors";
+    }
+    
+    return `${outcome} ${gameCondition}`
+}
+
+console.log(playRound(computerChoice, humanChoice));
+
 // Game is restarted
